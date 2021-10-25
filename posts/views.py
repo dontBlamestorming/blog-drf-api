bb30ad831059a0a9824856a3ac17fd4ac9ae4080
ukdong <lklone20005@gmail.com>
2021-10-26 00:54:17 +0900

    change GenericViewSet -> ViewSet && add CategoryViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,38 @@
 from .models import Category, Post
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework.response import Response
-from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from rest_framework import viewsets
 
-from .serializers import PostListSerializer
-
-from rest_framework.mixins import (
-    CreateModelMixin,
-    DestroyModelMixin,
-    ListModelMixin,
-    RetrieveModelMixin,
-)
+from .serializers import PostSerializer, CategorySerializer
 
 
-class PostViewSet(ListModelMixin, viewsets.GenericViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
+class PostViewSet(viewsets.ViewSet):
+    """ Whole post listing or retrieving by id """
+
+    # def list(self, request):
+    #     queryset = Post.objects.all()
+    #     serializer = PostListSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """ Category Model listing or retrieving """
+
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Category.objects.all()
+        category = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
