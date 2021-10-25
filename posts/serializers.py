327e2fad72ecbbbfc1e11ab990d42623a81a3ac4
ukdong <lklone20005@gmail.com>
2021-10-26 00:53:18 +0900

    add PostList, Post Serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,30 @@ from .models import Category, Post
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ('id', 'title', 'sub_title', 'thumbnail_url', 'category')
+
+    """
+        def to_representation(self, instance):
+            # present category by name, not id
+            rep = super().to_representation(instance)
+            rep['category'] = instance.category.name
+            return rep
+    """
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'post_date', 'content',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    posts = PostListSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Category
+        fields = '__all__'
